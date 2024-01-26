83514fddd5f58ae7bfe59a02ee66255ccafd126f
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-01-26 05:45:31 -0800

    complete my task 2

---
+++ task2.js
@@ -0,0 +1,10 @@
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+var Bangla = 60;
+var english =80;
+var math=40;
+var Physics=40;
+var Chemistry= 90;
+var total = Bangla+english+math+Physics+Chemistry
+console.log(total);
+var average=total/5;
+console.log( average);
